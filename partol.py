c1c8f9b6fc49864b0c6d18c5750a8cbaad7e16b4
KaiVerschuren <91733@roc-teraa.nl>
2025-09-10 13:38:10 +0200

    changed patrol back to no functions

--- partol.py
+++ partol.py
@@ -3,57 +3,66 @@ from arduino_alvik import ArduinoAlvik
 
 alvik = ArduinoAlvik()
 
-def pivot_90_right():
-  alvik.set_wheels_speed(30, -30)
-  delay(1350)
-
-def pivot_90_left():
-  alvik.set_wheels_speed(-30, 30)
-  delay(1350)
-
-def forward(R, t):
-  alvik.set_wheels_speed((R * 1000), (R * 1000))
-  delay(t)
-  alvik.set_wheels_speed(0, 0)
-
-def backward(R, t):
-  alvik.set_wheels_speed(-(R * 1000), -(R * 1000))
-  delay(t)
-  alvik.set_wheels_speed(0, 0)
-  
-
 def setup():
-  alvik.begin()
-  delay(1000)
+    alvik.begin()
+    delay(1000)
   
 def loop():
-  forward(30, 12)
-  
-  pivot_90_right()
-  
-  foward(30, 7)
-  
-  pivot_90_right()
-  
-  forward(30, 7)
-  
-  pivot_90_left()
-  pivot_90_left()
-  
-  forward(30, 7)
-  
-  pivot_90_left()
-  
-  forward(30, 7)
-  
-  pivot_90_left()
-  forward(30, 12)
-  
-  pivot_90_right()
-  pivot_90_right()
+    alvik.set_wheels_speed(30, 30)
+    delay(12000)
+    alvik.set_wheels_speed(0, 0)
 
+    alvik.set_wheels_speed(30, -30)
+    delay(1350)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(30, 30)
+    delay(7000)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(30, -30)
+    delay(1350)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(30, 30)
+    delay(7000)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(-30, 30)
+    delay(1350)
+    alvik.set_wheels_speed(0, 0)
+    alvik.set_wheels_speed(-30, 30)
+    delay(1350)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(30, 30)
+    delay(7000)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(-30, 30)
+    delay(1350)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(30, 30)
+    delay(7000)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(-30, 30)
+    delay(1350)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(30, 30)
+    delay(12000)
+    alvik.set_wheels_speed(0, 0)
+
+    alvik.set_wheels_speed(30, -30)
+    delay(1350)
+    alvik.set_wheels_speed(0, 0)
+    alvik.set_wheels_speed(30, -30)
+    delay(1350)
+    alvik.set_wheels_speed(0, 0)
 
 def cleanup():
-  alvik.stop()
+    alvik.stop()
   
 start(setup, loop, cleanup)
